c2bc0cf811e655b4bd816825a63e454fb7af8166
sbx_user1051 <sbx_user1051@169.254.99.205>
2024-07-14 16:57:49 +0000

    Sun Jul 14 16:57:49 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,14 @@
+"""hello_solution challenge."""
+
+
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
+    """Input friend name. Return a string containing a message.
+
+    :param friend_name: name of friend
+    :return: string containing a message
+    """
     if not isinstance(friend_name, str):
         raise TypeError("friend_name must be a string.")
     return "Hello World!"
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -8,8 +8,23 @@ class TestHello:
     @pytest.mark.parametrize(
         "friend_name, expected_message",
         [
-            ("tom", "Hello World!")
+            ("tom", "Hello World!"),
+            ("another_friend", "Hello World!"),
+            ("and_another_friend", "Hello World!"),
+            ("", "Hello World!"),
         ],
     )
     def test_hello(self, friend_name, expected_message):
         assert hello(friend_name) == expected_message
+
+    @pytest.mark.parametrize(
+        "friend_name, expected_exception_class",
+        [
+            (0.0, TypeError),
+            ({"a": "b"}, TypeError),
+            ({2, 3, 4}, TypeError),
+        ],
+    )
+    def test_hello_exceptions(self, friend_name, expected_exception_class):
+        with pytest.raises(expected_exception_class):
+            hello(friend_name)
