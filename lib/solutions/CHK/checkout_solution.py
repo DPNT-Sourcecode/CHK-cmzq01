f984c8058341a4abc9bcb461396ef5f679075422
sbx_user1051 <sbx_user1051@169.254.107.133>
2024-07-14 17:27:21 +0000

    Sun Jul 14 17:27:21 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def compute_price_of_single_item_type(quantity: int, single_price: int, offer: dict) -> int:
+    """Input item quanity, normal single item price, tuple describing item offer. Returns total price including offers.
+
+    :param quantity: number of items purchased
+    :param single_price: normal price of a single item (ie. without offer)
+    :param offer: offer in the form of a tuple (<quantity>, <total_price>)
+    :return: total price after offers applied
+    """
+    return (quantity // offer[0]) * offer[1] + (quantity % offer[0]) * single_price
+
+
 def checkout(skus):
     raise NotImplementedError()
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,15 @@
+import pytest
+
+from solutions.CHK.checkout_solution import compute_price_of_single_item_type, checkout
+
+
+class TestCheckout:
+
+    @pytest.mark.parametrize(
+        "quantity, single_price, offer, expected_price",
+        [
+            (10, 50, (3, 130), 440),
+        ],
+    )
+    def test_compute_price_of_single_item_type(self, quantity, single_price, offer, expected_price):
+        assert compute_price_of_single_item_type(quantity, single_price, offer) == expected_price
