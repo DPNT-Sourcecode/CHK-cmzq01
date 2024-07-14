d22be02b9ed3a796614b5d8f7a917191d85fd1e5
sbx_user1051 <sbx_user1051@169.254.107.133>
2024-07-14 17:39:21 +0000

    Sun Jul 14 17:39:21 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,22 +1,24 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-def compute_price_of_single_item_type(quantity: int, single_price: int, offer: tuple[int, int] = None) -> int:
+def compute_price_of_single_item_type(quantity: int, single_item_price: int, offer: tuple[int, int] = None) -> int:
     """Input item quanity, normal single item price, tuple describing item offer. Returns total price including offers.
 
     :param quantity: number of items purchased
-    :param single_price: normal price of a single item (ie. without offer)
+    :param single_item_price: normal price of a single item (ie. without offer)
     :param offer: offer in the form of a tuple (<quantity>, <total_price>). Pass None if there is no offer.
     :return: total price after offers applied
     """
+    if not (isinstance(quantity, int) and isinstance(single_item_price, int)):
+        raise TypeError("quantity and single_item_price must be integers")
     if offer is None:
-        return quantity * single_price
-    if not len(offer) == 2:
+        return quantity * single_item_price
+    elif not len(offer) == 2:
         raise IndexError("offer must be of the form (<quantity>, <total_price>)")
     elif not (isinstance(offer[0], int) and isinstance(offer[1], int)):
         raise TypeError("offer must be a tuple with types (int, int)")
     else:
-        return (quantity // offer[0]) * offer[1] + (quantity % offer[0]) * single_price
+        return (quantity // offer[0]) * offer[1] + (quantity % offer[0]) * single_item_price
 
 
 def checkout(skus):
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,7 +6,7 @@ from solutions.CHK.checkout_solution import compute_price_of_single_item_type, c
 class TestCheckout:
 
     @pytest.mark.parametrize(
-        "quantity, single_price, offer, expected_price",
+        "quantity, single_item_price, offer, expected_price",
         [
             (10, 50, (3, 130), 440),
             (6, 30, (2, 45), 135),
@@ -14,6 +14,21 @@ class TestCheckout:
             (4, 15, None, 60),
         ],
     )
-    def test_compute_price_of_single_item_type(self, quantity, single_price, offer, expected_price):
-        assert compute_price_of_single_item_type(quantity, single_price, offer) == expected_price
+    def test_compute_price_of_single_item_type(self, quantity, single_item_price, offer, expected_price):
+        assert compute_price_of_single_item_type(quantity, single_item_price, offer) == expected_price
+
+    @pytest.mark.parametrize(
+        "quantity, single_item_price, offer, expected_exception_class",
+        [
+            ("ten", 50, None, TypeError),
+            (10, 50.0, None, TypeError),
+            (10, 50, (3), IndexError),
+            (10, 50, (3, "wrong"), TypeError),
+            (10, 50, (3.0, 5), TypeError),
+            (10, 50, (3, 5, "wrong"), IndexError),
+        ],
+    )
+    def test_compute_price_of_single_item_type_exceptions(self, quantity, single_item_price, offer, expected_exception_class):
+        with pytest.raises(expected_exception_class):
+            compute_price_of_single_item_type(quantity, single_item_price, offer)
 
