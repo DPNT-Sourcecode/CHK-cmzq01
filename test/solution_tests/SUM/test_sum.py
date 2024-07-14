e37ce19b75fdd90b12fedea4744d9fd49a567cf5
sbx_user1051 <sbx_user1051@169.254.56.101>
2024-07-14 11:21:39 +0000

    Sun Jul 14 11:21:39 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,6 @@
 import pytest
 
-from solutions.SUM import sum_solution
+from solutions.SUM.sum_solution import InputOutOfRangeError, compute
 
 
 class TestSum:
@@ -14,7 +14,7 @@ class TestSum:
         ]
     )
     def test_sum(self, x, y, expected_sum):
-        assert sum_solution.compute(x, y) == expected_sum
+        assert compute(x, y) == expected_sum
 
     @pytest.mark.parametrize(
         "x, y, expected_exception_class",
@@ -22,12 +22,12 @@ class TestSum:
             (0.0, 0.0, TypeError),
             (15.6, 5, TypeError),
             (5, 15.6, TypeError),
-            (-1, 100, sum_solution.InputOutOfRangeError),
-            (101, 100, sum_solution.InputOutOfRangeError),
-            (100, 101, sum_solution.InputOutOfRangeError),
-            (100, -1, sum_solution.InputOutOfRangeError),
+            (-1, 100, InputOutOfRangeError),
+            (101, 100, InputOutOfRangeError),
+            (100, 101, InputOutOfRangeError),
+            (100, -1, InputOutOfRangeError),
         ]
     )
     def test_sum_exceptions(self, x, y, expected_exception_class):
         with pytest.raises(expected_exception_class):
-            sum_solution.compute(x, y)
+            compute(x, y)
