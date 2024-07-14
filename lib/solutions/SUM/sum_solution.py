1d30aea75730b694403b6c8cd18661111ea0f937
sbx_user1051 <sbx_user1051@169.254.56.101>
2024-07-14 11:18:39 +0000

    Sun Jul 14 11:18:39 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -14,10 +14,14 @@ def compute(x: int, y: int) -> int:
     :param y: Second integer
     :return: Sum of x and y
     """
+
+    # x and y must be integers, or raise TypeError.
     if x is not int or y is not int:
         raise TypeError("Inputs x and y must both be integers.")
+
+    # x and y must be between 0 and 100 inclusive, or raise InputOutOfRangeError.
+    elif not (0 <= x <= 100 and 0 <= y <= 100):
+        raise InputOutOfRangeError()
+
+    # Return sum.
     return x + y
-
-
-
-
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -17,7 +17,7 @@ class TestSum:
         assert sum_solution.compute(x, y) == expected_sum
 
     @pytest.mark.parametrize(
-        "x, y, expected_exception",
+        "x, y, expected_exception_class",
         [
             (0.0, 0.0, TypeError),
             (15.6, 5, TypeError),
@@ -28,9 +28,6 @@ class TestSum:
             (100, -1, sum_solution.InputOutOfRangeError),
         ]
     )
-    def test_sum_exceptions(self, x, y, expected_exception):
-        with pytest.raises(TypeError):
+    def test_sum_exceptions(self, x, y, expected_exception_class):
+        with pytest.raises(expected_exception_class):
             sum_solution.compute(x, y)
-
-
-
