17101f581147fdd0bd670604a7884891c7a51d2c
sbx_user1051 <sbx_user1051@169.254.82.245>
2024-07-15 18:35:03 +0000

    Mon Jul 15 18:35:03 UTC 2024

--- test/solution_tests/CHK/test_offers.py
+++ test/solution_tests/CHK/test_offers.py
@@ -27,11 +27,25 @@ class TestOffers:
             (10, 5.5, TypeError),
         ],
     )
-    def test_calculate_price_single_product_offer(
+    def test_calculate_price_single_product_offer_exceptions(
             self, quantity, single_unit_price, expected_exception_class
     ):
         with pytest.raises(expected_exception_class):
             offer = SingleProductOffer(single_unit_price)
             offer.calculate_price(quantity)
+
+    @pytest.mark.parametrize(
+        "quantity, single_unit_price, buy_quantity, get_quantity, expected_price",
+        [
+            (1, 10, 2, 1, 10),
+            (2, 10, 2, 1, 20),
+            (3, 10, 2, 1, 20),
+        ],
+    )
+    def test_calculate_price_bgf_offer(
+            self, quantity, single_unit_price, buy_quantity, get_quantity, expected_price
+    ):
+        offer = BgfOffer(single_unit_price, buy_quantity, get_quantity)
+        assert offer.calculate_price(quantity) == expected_price
 
 
