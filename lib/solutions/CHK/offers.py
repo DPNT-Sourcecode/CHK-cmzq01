c7a75a11836f99b5bda2b279b001604b7323a01e
sbx_user1051 <sbx_user1051@169.254.82.245>
2024-07-15 18:26:03 +0000

    Mon Jul 15 18:26:03 UTC 2024

---
+++ lib/solutions/CHK/offers.py
@@ -0,0 +1,17 @@
+class SingleProductOffer:
+    def __init__(self, single_unit_price: int):
+        self.single_unit_price = single_unit_price
+
+    def calculate_price(self, quantity: int) -> int:
+        return self.single_unit_price * quantity
+
+
+class BgfOffer(SingleProductOffer):
+    def __init__(self, single_unit_price: int, buy_quantity: int, get_quantity: int):
+        super().__init__(single_unit_price)
+        self.buy_quantity = buy_quantity
+        self.get_quantity = get_quantity
+
+    def calculate_price(self, quantity: int) -> int:
+        return (quantity - quantity // (self.buy_quantity + self.get_quantity)) * self.single_unit_price
+
