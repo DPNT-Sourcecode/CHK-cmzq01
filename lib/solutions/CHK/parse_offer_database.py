43e4ebdaa23565152c10e8b96b5f6edaba6f1855
sbx_user1051 <sbx_user1051@169.254.73.5>
2024-07-16 10:06:33 +0000

    Tue Jul 16 10:06:33 UTC 2024

--- lib/solutions/CHK/parse_offer_database.py
+++ lib/solutions/CHK/parse_offer_database.py
@@ -2,7 +2,7 @@ import re
 
 from CHK.offers import SingleProductOffer, CrossProductOffer, LadderOffer, LadderDiscount, BgfOffer
 
-single_ladder_pattern = r"\|\s*([A-Z])\s*\|\s*(\d+)\s*\|\s*(\d+)[A-Z]\s+for\s+(\d+),\s*(\d+)([A-Z])\s+for\s+(\d+)\s*\|"
+single_ladder_pattern = r"\|\s*([A-Z])\s*\|\s*(\d+)\s*\|\s*(\d+)[A-Z]\s+for\s+(\d+)\s*\|"
 double_ladder_pattern = r"\|\s*([A-Z])\s*\|\s*(\d+)\s*\|\s*(\d+)[A-Z]\s+for\s+(\d+),\s*(\d+)[A-Z]\s+for\s+(\d+)\s*\|"
 cross_product_and_bgf_pattern = r"\|\s*([A-Z])\s*\|\s*(\d+)\s*\|\s*(\d+)[A-Z]\s+get\s+one\s+([A-Z])\s+free\s*\|"
 
@@ -15,7 +15,7 @@ def parse_line(line) -> tuple[str, [SingleProductOffer, CrossProductOffer]]:
     if re.match(single_ladder_pattern, line):
         sku, single_unit_price, discount_quantity, discount_price = re.findall(single_ladder_pattern, line)[0]
         return_sku = sku
-        return_offer = LadderOffer(single_unit_price, [LadderDiscount(discount_quantity, discount_price)])
+        return_offer = LadderOffer(int(single_unit_price), [LadderDiscount(int(discount_quantity), int(discount_price))])
     elif re.match(double_ladder_pattern, line):
         sku, single_unit_price, discount_1_quantity, discount_1_price, discount_2_quantity, discount_2_price = re.findall(double_ladder_pattern, line)[0]
         return_sku = sku
@@ -35,6 +35,7 @@ def parse_line(line) -> tuple[str, [SingleProductOffer, CrossProductOffer]]:
             return_offer = CrossProductOffer(single_unit_price, subject_sku, buy_quantity, target_sku)
 
     return return_sku, return_offer
+
 
 
 
