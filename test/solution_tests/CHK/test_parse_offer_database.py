ab162a312b63595fbb6ef2e1b452f2fb5403d75b
sbx_user1051 <sbx_user1051@169.254.73.5>
2024-07-16 10:00:33 +0000

    Tue Jul 16 10:00:33 UTC 2024

--- test/solution_tests/CHK/test_parse_offer_database.py
+++ test/solution_tests/CHK/test_parse_offer_database.py
@@ -1,6 +1,8 @@
 import pytest
 
-from solutions.CHK.parse_offer_database import *
+from solutions.CHK.parse_offer_database import parse_line
+from solutions.CHK.offers import *
+
 
 
 class TestParseOfferDatabase:
@@ -8,12 +10,22 @@ class TestParseOfferDatabase:
     @pytest.mark.parametrize(
         "line, expected_sku, expected_offer",
         [
-            ("| B    | 30    | 2B for 45              |", "B", LadderOffer(30, [LadderDiscount(2, 45)])),
+            (
+                    "| B    | 30    | 2B for 45              |",
+                    "B",
+                    LadderOffer(
+                        30,
+                        [
+                            LadderDiscount(2, 45)
+                        ]
+                    )
+            ),
         ],
     )
     def test_parse_line(
             self, line, expected_sku, expected_offer
     ):
-        sku, offer = parse_line(line)
+        d= parse_line(line)
         assert sku == expected_sku
         assert offer == expected_offer
+
