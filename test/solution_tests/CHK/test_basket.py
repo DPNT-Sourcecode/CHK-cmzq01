9ed279ca91f813f0bc9c8342785870ecbc68d4e9
sbx_user1051 <sbx_user1051@169.254.98.189>
2024-07-22 08:12:18 +0000

    Mon Jul 22 08:12:18 UTC 2024

--- test/solution_tests/CHK/test_basket.py
+++ test/solution_tests/CHK/test_basket.py
@@ -1,7 +1,7 @@
 import pytest
 
 from solutions.CHK.basket import *
-from solutions.CHK.offers import CrossProductOffer, LadderOffer, LadderDiscount, BgfOffer, SingleProductOffer
+
 # useful: python commands to generate random SKUs
 # from string import ascii_uppercase
 # "".join([f"\"{char}\" * {randint(0,20)}{' + ' if i != len(ascii_uppercase) - 1 else ''}" \
@@ -67,5 +67,6 @@ class TestBasket:
         basket = Basket(skus, small_offer_database)
         assert basket.basket_contents.__hash__ == expected_basket_contents.__hash__
         assert basket.final_price == expected_final_price
+
 
 
